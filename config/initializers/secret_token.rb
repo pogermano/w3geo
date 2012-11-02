cd2a4d953da2fd803cfe12e160894b45a80afd20
pedro germano <pogermano@gmail.com>
2012-11-02 12:36:58 -0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+W3geo::Application.config.secret_token = '5aa21516cff3bbfd81a91ab35cdea06e284f250349b5d79f2fa500de44c0e8302ddfc8c33e52e65b47f94f55de0857ce1fd91857891886bb95bdbfdd3f4dc546'
