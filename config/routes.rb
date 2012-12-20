8d2f77a9dccfced1956310e9c20dd4febbbb7d34
pedro germano <pogermano@gmail.com>
2012-12-20 16:08:45 -0200

    Fix signing up feature, take user to alternative confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 W3geo::Application.routes.draw do
 
 
-   devise_for :users 
+   devise_for :users, :controllers => { :registrations => "registrations" }
+   
+    get '/awaiting_confirmation',
+         :to => "users#confirmation",
+         :as => 'confirm_user'
+
+
    namespace :admin do
       root :to => "base#index"
       resources :users
