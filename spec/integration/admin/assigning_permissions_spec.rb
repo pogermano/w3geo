1d4a2bd53dd67eaf08bcc390bbc20dbbe66a4bd3
pedro germano <pogermano@gmail.com>
2012-12-24 15:34:35 -0200

    Add creating, editing, updating and deleting tickets to assigning permissions interface

--- spec/integration/admin/assigning_permissions_spec.rb
+++ spec/integration/admin/assigning_permissions_spec.rb
@@ -24,5 +24,51 @@ feature "Assigning permissions" do
     sign_in_as!(user)
     page.should have_content(customer.name)
   end
+
+  scenario "Creating tickets for a customer" do
+    check_permission_box "view", customer
+    check_permission_box "create_tickets", customer
+    click_button "Update"
+    click_link "Sign out"
+    sign_in_as!(user)
+    click_link customer.name
+    click_link "New Ticket"
+    fill_in "Title", :with => "Shiny!"
+    fill_in "Description", :with => "Make it so!"
+    click_button "Create"
+    page.should have_content("Ticket has been created.")
+  end
+
+  scenario "Updating a ticket for a customer" do
+    check_permission_box "view", customer
+    check_permission_box "edit_tickets", customer
+    click_button "Update"
+    click_link "Sign out"
+    sign_in_as!(user)
+    click_link customer.name
+    click_link ticket.title
+    click_link "Edit Ticket"
+    fill_in "Title", :with => "Really shiny!"
+    click_button "Update Ticket"
+    page.should have_content("Ticket has been updated")
+  end
+
+
+
+  scenario "Deleting a ticket for a customer" do
+    check_permission_box "view", customer
+    check_permission_box "delete_tickets", customer
+    click_button "Update"
+    click_link "Sign out"
+    sign_in_as!(user)
+    click_link customer.name
+    click_link ticket.title
+    click_link "Delete Ticket"
+    page.should have_content("Ticket has been deleted.")
+  end
+
+
+
+
 end
 
